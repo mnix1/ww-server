9e29cd35915ce3bf47736fe8cb3dca3111932a43
mnix1 <mvppl123@gmail.com>
2018-11-18 19:10:37 +0100

    upgredes

--- src/main/java/com/ww/controller/PromoController.java
+++ src/main/java/com/ww/controller/PromoController.java
@@ -9,7 +9,6 @@ import com.ww.model.entity.outside.social.Profile;
 import com.ww.service.wisie.ProfileWisieService;
 import com.ww.service.social.ProfileService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/promo")
+@RequestMapping(value = "/_promo")
 @AllArgsConstructor
 public class PromoController {
 
