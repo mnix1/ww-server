38080633d645d4e2551beb761c80478433606d7e
mnix1 <mvppl123@gmail.com>
2018-11-22 01:14:45 +0100

    season grade rewards

--- src/main/java/com/ww/database/InitGradeService.java
+++ src/main/java/com/ww/database/InitGradeService.java
@@ -23,18 +23,18 @@ public class InitGradeService {
 
     public void initSeasonGrades() {
         List<SeasonGrade> seasonGrades = new ArrayList<>();
-        seasonGrades.add(new SeasonGrade(Grade.A, RivalType.WAR, 1600L, null, new Resources(60L, 40L, 40L, 80L)));
-        seasonGrades.add(new SeasonGrade(Grade.B, RivalType.WAR, 800L, 1599L, new Resources(30L, 20L, 20L, 30L)));
-        seasonGrades.add(new SeasonGrade(Grade.C, RivalType.WAR, 400L, 799L, new Resources(15L, 10L, 10L, 10L)));
-        seasonGrades.add(new SeasonGrade(Grade.D, RivalType.WAR, 200L, 399L, new Resources(10L, 5L, 5L, 4L)));
-        seasonGrades.add(new SeasonGrade(Grade.E, RivalType.WAR, 100L, 199L, new Resources(5L, 2L, 2L, 2L)));
+        seasonGrades.add(new SeasonGrade(Grade.A, RivalType.WAR, 1600L, null, new Resources(100L, 80L, 80L, 120L)));
+        seasonGrades.add(new SeasonGrade(Grade.B, RivalType.WAR, 800L, 1599L, new Resources(50L, 40L, 40L, 60L)));
+        seasonGrades.add(new SeasonGrade(Grade.C, RivalType.WAR, 400L, 799L, new Resources(25L, 20L, 20L, 30L)));
+        seasonGrades.add(new SeasonGrade(Grade.D, RivalType.WAR, 200L, 399L, new Resources(13L, 10L, 10L, 15L)));
+        seasonGrades.add(new SeasonGrade(Grade.E, RivalType.WAR, 100L, 199L, new Resources(7L, 5L, 5L, 8L)));
         seasonGrades.add(new SeasonGrade(Grade.F, RivalType.WAR, 0L, 99L, new Resources(0L, 0L, 0L, 0L)));
 
-        seasonGrades.add(new SeasonGrade(Grade.A, RivalType.BATTLE, 2000L, null, new Resources(30L, 20L, 20L, 40L)));
-        seasonGrades.add(new SeasonGrade(Grade.B, RivalType.BATTLE, 1000L, 1099L, new Resources(15L, 10L, 10L, 15L)));
-        seasonGrades.add(new SeasonGrade(Grade.C, RivalType.BATTLE, 500L, 999L, new Resources(7L, 5L, 5L, 5L)));
-        seasonGrades.add(new SeasonGrade(Grade.D, RivalType.BATTLE, 250L, 499L, new Resources(5L, 2L, 2L, 2L)));
-        seasonGrades.add(new SeasonGrade(Grade.E, RivalType.BATTLE, 125L, 124L, new Resources(2L, 1L, 1L, 1L)));
+        seasonGrades.add(new SeasonGrade(Grade.A, RivalType.BATTLE, 2000L, null, new Resources(60L, 40L, 40L, 60L)));
+        seasonGrades.add(new SeasonGrade(Grade.B, RivalType.BATTLE, 1000L, 1099L, new Resources(30L, 20L, 20L, 30L)));
+        seasonGrades.add(new SeasonGrade(Grade.C, RivalType.BATTLE, 500L, 999L, new Resources(15L, 10L, 10L, 15L)));
+        seasonGrades.add(new SeasonGrade(Grade.D, RivalType.BATTLE, 250L, 499L, new Resources(8L, 5L, 5L, 8L)));
+        seasonGrades.add(new SeasonGrade(Grade.E, RivalType.BATTLE, 125L, 124L, new Resources(4L, 3L, 3L, 4L)));
         seasonGrades.add(new SeasonGrade(Grade.F, RivalType.BATTLE, 0L, 124L, new Resources(0L, 0L, 0L, 0L)));
         seasonGradeRepository.saveAll(seasonGrades);
     }
