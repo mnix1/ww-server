cd0604e60a401fa8bfdb68fdc7a904b213c19c69
mnix1 <mvppl123@gmail.com>
2018-12-01 19:32:13 +0100

    json helper

--- src/main/java/com/ww/helper/JSONHelper.java
+++ src/main/java/com/ww/helper/JSONHelper.java
@@ -3,7 +3,7 @@ package com.ww.helper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class JSONHelper2 {
+public class JSONHelper {
     public static String toJSON(Object o) {
         ObjectMapper mapper = new ObjectMapper();
         try {
